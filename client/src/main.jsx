4c38c9dab6f42b0712d13a5d3060265fa345030d
Viktor Timofeev <l_893@mail.ru>
2025-11-26 16:16:55 +0400

    chore(app): reorder imports to ensure CSS foundation loads before App

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { BrowserRouter } from 'react-router-dom';
-import { App } from './app/app';
 import './normalize.css';
 import './index.css';
+import { App } from './app/app';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
