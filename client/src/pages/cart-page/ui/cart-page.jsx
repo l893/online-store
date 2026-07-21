3228bb5bc0ada6a7930b5a7acf4352d87e6d5b77
Viktor Timofeev <l_893@mail.ru>
2026-07-21 10:03:38 +0400

    refactor(pages): organize cart page ui segment

--- client/src/pages/cart-page/ui/cart-page.jsx
+++ client/src/pages/cart-page/ui/cart-page.jsx
@@ -5,11 +5,11 @@ import {
   useGetCartQuery,
   useInitialCartSync,
   useReplaceCartMutation,
-} from '../../features/cart';
-import { AlertDialog, Loader } from '../../shared/ui';
-import { CartItem } from '../../widgets/cart-item';
-import { CartSummary } from '../../widgets/cart-summary';
-import { useCartCheckout } from './model/use-cart-checkout';
+} from '../../../features/cart';
+import { AlertDialog, Loader } from '../../../shared/ui';
+import { CartItem } from '../../../widgets/cart-item';
+import { CartSummary } from '../../../widgets/cart-summary';
+import { useCartCheckout } from '../model/use-cart-checkout';
 import styles from './cart-page.module.scss';
 
 export const CartPage = () => {
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,6 @@
 export * from './catalog-page/catalog-page';
 export * from './product-page';
-export * from './cart-page/cart-page';
+export * from './cart-page';
 export * from './login-page';
 export * from './register-page';
 export * from './admin-products-page/admin-products-page';
