4a8ac824be2db4139b600219bac777f0bb557459
Viktor Timofeev <l_893@mail.ru>
2026-07-29 19:40:34 +0400

    refactor(cart): hide initial sync decision helper

--- client/src/features/cart/index.js
+++ client/src/features/cart/index.js
@@ -2,7 +2,6 @@ export { default as cartReducer } from './model/cart.slice';
 export * from './model/cart.slice';
 export * from './api/cart.api';
 export * from './lib/get-cart-totals';
-export * from './lib/get-initial-cart-sync-decision';
 export * from './lib/merge-cart-items';
 export * from './model/add-product-to-cart';
 export * from './model/use-cart-item-actions';
