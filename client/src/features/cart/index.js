634e2196e5598585cd4c742d224246701f423e37
Viktor Timofeev <l_893@mail.ru>
2026-07-30 08:20:38 +0400

    refactor(cart): define cart public api

--- client/src/features/cart/index.js
+++ client/src/features/cart/index.js
@@ -1,8 +1,17 @@
-export { default as cartReducer } from './model/cart.slice';
-export * from './model/cart.slice';
-export * from './api/cart.api';
-export * from './lib/get-cart-totals';
-export * from './lib/merge-cart-items';
-export * from './model/add-product-to-cart';
-export * from './model/use-cart-item-actions';
-export * from './model/use-initial-cart-sync';
+export {
+  default as cartReducer,
+  clearCart,
+  setCartItems,
+} from './model/cart.slice';
+
+export {
+  useGetCartQuery,
+  useLazyGetCartQuery,
+  useReplaceCartMutation,
+} from './api/cart.api';
+
+export { getCartTotals } from './lib/get-cart-totals';
+export { mergeCartItems } from './lib/merge-cart-items';
+export { addProductToCart } from './model/add-product-to-cart';
+export { useCartItemActions } from './model/use-cart-item-actions';
+export { useInitialCartSync } from './model/use-initial-cart-sync';
