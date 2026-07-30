dc6b56b19113478e7eecaca3a6bd70b2182dc7e0
Viktor Timofeev <l_893@mail.ru>
2026-07-30 08:58:19 +0400

    refactor(admin-products): define public api

--- client/src/features/admin-products/index.js
+++ client/src/features/admin-products/index.js
@@ -1,7 +1,14 @@
-export * from './api/products.api';
-export * from './lib/create-product-form-initial-values';
-export * from './lib/create-product-payload';
-export * from './ui/admin-product-form-panel';
-export * from './ui/admin-products-pagination';
-export * from './ui/admin-products-table';
-export * from './ui/admin-products-toolbar';
+export {
+  useAdminListProductsQuery,
+  useAdminCreateProductMutation,
+  useAdminUpdateProductMutation,
+  useAdminDeleteProductMutation,
+} from './api/products.api';
+
+export { createProductFormInitialValues } from './lib/create-product-form-initial-values';
+export { createProductPayload } from './lib/create-product-payload';
+
+export { AdminProductFormPanel } from './ui/admin-product-form-panel';
+export { AdminProductsPagination } from './ui/admin-products-pagination';
+export { AdminProductsTable } from './ui/admin-products-table';
+export { AdminProductsToolbar } from './ui/admin-products-toolbar';
