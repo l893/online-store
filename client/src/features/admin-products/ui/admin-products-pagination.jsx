fd762c38228236d45543935b9dd9dd27eaf7d93d
Viktor Timofeev <l_893@mail.ru>
2026-08-12 15:44:55 +0400

    refactor(admin-products): use fsd alias in pagination

--- client/src/features/admin-products/ui/admin-products-pagination.jsx
+++ client/src/features/admin-products/ui/admin-products-pagination.jsx
@@ -1,4 +1,4 @@
-import { Button } from '../../../shared/ui';
+import { Button } from '@shared/ui';
 import styles from './admin-products-pagination.module.scss';
 
 export const AdminProductsPagination = ({
