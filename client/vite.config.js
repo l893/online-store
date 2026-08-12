2cbb32528fdc24f3af80f6089f11933cca977924
Viktor Timofeev <l_893@mail.ru>
2026-08-12 07:05:31 +0400

    build(client): configure fsd aliases

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,21 @@
+import { fileURLToPath, URL } from 'node:url';
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@app': fileURLToPath(new URL('./src/app', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@widgets': fileURLToPath(new URL('./src/widgets', import.meta.url)),
+      '@features': fileURLToPath(new URL('./src/features', import.meta.url)),
+      '@entities': fileURLToPath(new URL('./src/entities', import.meta.url)),
+      '@shared': fileURLToPath(new URL('./src/shared', import.meta.url)),
+    },
+  },
   build: {
     sourcemap: 'hidden',
   },
