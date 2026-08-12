97af8abc9c02d0564dc38b987385fd6fcc1c30ae
Viktor Timofeev <l_893@mail.ru>
2026-08-12 15:31:59 +0400

    refactor(cart): use fsd alias in cart item actions

--- client/src/features/cart/model/use-cart-item-actions.js
+++ client/src/features/cart/model/use-cart-item-actions.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { api } from '../../../shared/lib';
+import { api } from '@shared/lib';
 import { useRemoveItemFromCartMutation } from '../api/cart.api';
 import {
   changeCartItemQuantity,
