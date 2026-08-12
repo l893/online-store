770a1851b8c989073b5ec97d177aa69317122265
Viktor Timofeev <l_893@mail.ru>
2026-08-12 08:53:38 +0400

    refactor(cart): use fsd aliases in cart page

--- client/src/pages/cart-page/ui/cart-page.jsx
+++ client/src/pages/cart-page/ui/cart-page.jsx
@@ -1,8 +1,8 @@
 import { useSelector } from 'react-redux';
-import { useCartItemActions } from '../../../features/cart';
-import { AlertDialog, Loader } from '../../../shared/ui';
-import { CartItem } from '../../../widgets/cart-item';
-import { CartSummary } from '../../../widgets/cart-summary';
+import { useCartItemActions } from '@features/cart';
+import { AlertDialog, Loader } from '@shared/ui';
+import { CartItem } from '@widgets/cart-item';
+import { CartSummary } from '@widgets/cart-summary';
 import { useCartCheckout } from '../model/use-cart-checkout';
 import { useCartPageState } from '../model/use-cart-page-state';
 import styles from './cart-page.module.scss';
